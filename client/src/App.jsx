86411711686354d191c14677692372163ce2e033
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-26 11:17:01 +0600

    Create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Chatbox from "./components/Chatbox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World! How are you?</h1>
-    </div>
-  )
+    <>
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Chatbox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
