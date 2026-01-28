dd8a0830bd487241fc7778371bedf5e153a8f56b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-28 13:01:35 +0600

    Added loader

--- client/src/components/Chatbox.jsx
+++ client/src/components/Chatbox.jsx
@@ -37,6 +37,15 @@ function Chatbox() {
         {messages?.map((message, index) => (
           <Message key={index} message={message} />
         ))}
+
+        {/* Three Dots Loading */}
+{
+  loading && <div className='loader flex items-center gap-1.5'>
+    <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+    <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+    <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+  </div>
+}
       </div>
 
       {/* Prompt input */}
