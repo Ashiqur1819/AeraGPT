be82b63a806dda34b21bbfb4e4332875cfdcf50d
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-29 05:30:44 +0600

    Create basic express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express())
+
+// Routes
+app.get("/", (req, res) => res.send("Server is Live!"))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
