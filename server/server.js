90a0ae910409447e5e53beef802cb691efbd7b19
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2026-01-30 11:11:40 +0600

    Connected mongodb database by mongoose

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express"
 import cors from "cors"
+import connectDB from "./configs/db.js"
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+await connectDB()
 
 // Middlewares
 app.use(cors())
